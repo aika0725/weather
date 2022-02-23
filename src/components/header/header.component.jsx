189f97933a044453fb695320611aa481d3abade7
aika0725 <yfxx@live.com>
2022-02-23 09:54:00 +0100

    add icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import SearchBar from "../search-bar/search-bar.component";
 
 const Header = (props) =>{
-    
+    console.log('hi')
     return(
     <div className="header">
+
         <SearchBar
             placeholder='City name'
             setCity={props.setCity}
--- src/components/weather-showcase/weather-showcase.component.jsx
+++ src/components/weather-showcase/weather-showcase.component.jsx
@@ -1,28 +1,36 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-const WeatherShowcase =(props)=> { //props.cityName
+const WeatherShowcase = (props) => { //props.cityName
     const [weather, setWeather] = useState(null)
     const [country, setCountry] = useState(null)
     const [city, setCity] = useState(null)
-    useEffect(() =>{
-        const fetchFunc = async()=>{
-           const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=933d6b84e5a54ccda95231846222202&q=${props.cityName}&aqi=no`)
-           const resJson = await response.json()
-           console.log(resJson.current.temp_c)
-           setWeather(resJson.current.temp_c)
-           setCountry(resJson.location.country)
-           setCity(resJson.location.name)
-        }  
+    const [icon, setIcon] = useState(null)
+    const [condition, setCondition] = useState(null)
+
+    useEffect(() => {
+        const fetchFunc = async () => {
+            const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=933d6b84e5a54ccda95231846222202&q=${props.cityName}&aqi=no`)
+            const resJson = await response.json()
+            setIcon(resJson.current.condition.icon)
+            setCondition(resJson.current.condition.text)
+            setWeather(resJson.current.temp_c)
+            setCountry(resJson.location.country)
+            setCity(resJson.location.name)
+            console.log(resJson)
+        }
         fetchFunc()
-    },[props.cityName])
-    return(
-    <div 
-    className="weather-showcase"
-    >
-        <p>{city}, {country}</p>
-        <h2>{weather}℃</h2>
-        
-    </div>
-)}
+    }, [props.cityName])
+
+    return (
+        <div
+            className="weather-showcase"
+        >
+            <p>{city}, {country}</p>
+            <h2>{weather}℃</h2>
+            <img src={`http:${icon}`} alt={condition}/>
+
+        </div>
+    )
+}
 
 export default WeatherShowcase
